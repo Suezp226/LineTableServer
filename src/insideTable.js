b468d316f60a9e451f7deabdc4e67a69b430c4ea
Suezp <769198688@qq.com>
2021-08-12 23:12:41 +0800

    编辑、删除功能完成，查询功能未完善

--- src/insideTable.js
+++ src/insideTable.js
@@ -10,14 +10,14 @@ const table = tableObj.table;
 
 //  以下两个都是需要考 传参来搜索表格的id来实现
 
-const getInsideTableSchema = function (schemaList) {
+const getInsideTableSchema = function(schemaList) {
 
-  let sche = {};
-  schemaList.forEach(ele => {
-    sche[ele] = String;
-  })
+    let sche = {};
+    schemaList.forEach(ele => {
+        sche[ele] = String;
+    })
 
-  return mongoose.Schema(sche,{ strict: false })
+    return mongoose.Schema(sche, { strict: false })
 }
 
 /*    ----------请求处理--------- */
@@ -25,7 +25,7 @@ const getInsideTableSchema = function (schemaList) {
 // 路由拦截器
 
 router.all('*', (req, res, next) => {
-  next();
+    next();
 })
 
 // 用来保存insideTable model   因为 同一个model 不能重复定义
@@ -34,23 +34,23 @@ let insideTable = {};
 // ------ GET 请求 
 // 获取表格数据
 router.get('/getInsideData', (req, res, next) => {
-  let query = req.query;
-  table.findOne({ ...query }, {}, (err, docs) => {
-    let insideTableInfo = getInsideTableSchema(docs.tagSchema);
-    let nowModel = insideTable[('lineTableData' + docs._id)];
-    
-    // 不存在这个 model 的时候 添加
-    if(!nowModel) {
-      insideTable[('lineTableData' + docs._id)] = mongoose.model('lineTableData' + docs._id, insideTableInfo);
-      nowModel = insideTable[('lineTableData' + docs._id)];
-    };
-    nowModel.find({}, {}, (err, docs) => {
-      if(err) {
-        console.log(err)
-      }
-      res.send({ code: 200, docs })
+    let query = req.query;
+    table.findOne({...query }, {}, (err, docs) => {
+        let insideTableInfo = getInsideTableSchema(docs.tagSchema);
+        let nowModel = insideTable[('lineTableData' + docs._id)];
+
+        // 不存在这个 model 的时候 添加
+        if (!nowModel) {
+            insideTable[('lineTableData' + docs._id)] = mongoose.model('lineTableData' + docs._id, insideTableInfo);
+            nowModel = insideTable[('lineTableData' + docs._id)];
+        };
+        nowModel.find({}, {}, (err, docs) => {
+            if (err) {
+                console.log(err)
+            }
+            res.send({ code: 200, docs })
+        })
     })
-  })
 })
 
 
@@ -62,34 +62,85 @@ router.use(express.urlencoded({ extended: true }));
 
 //新增数据
 router.post('/addInsideData', (req, res, next) => {
-  let query = req.body;
-  let id = query._id;  // 总表id
-  table.findOne({ _id: id }, {}, (err, docs) => {
-    let insideTableInfo = getInsideTableSchema(docs.tagSchema)
-    let nowModel = insideTable[('lineTableData' + docs._id)];
-    // 不存在这个 model 的时候 添加
-    if(!nowModel) {
-      insideTable[('lineTableData' + docs._id)] = mongoose.model('lineTableData' + docs._id, insideTableInfo);
-      nowModel = insideTable[('lineTableData' + docs._id)];
-    };
-    let param = {...query};
-    delete param._id;   //删掉该表的 id
-    nowModel.create([param], (err) => {
-      if(!err) {
-        res.send({code:200,msg:'添加成功'})
-      } else {
-        res.send({code:301,msg:err});
-      }
+    let query = req.body;
+    let id = query._id; // 总表id
+    table.findOne({ _id: id }, {}, (err, docs) => {
+        let insideTableInfo = getInsideTableSchema(docs.tagSchema)
+        let nowModel = insideTable[('lineTableData' + docs._id)];
+        // 不存在这个 model 的时候 添加
+        if (!nowModel) {
+            insideTable[('lineTableData' + docs._id)] = mongoose.model('lineTableData' + docs._id, insideTableInfo);
+            nowModel = insideTable[('lineTableData' + docs._id)];
+        };
+        let param = {...query };
+        delete param._id; //删掉该表的 id
+        nowModel.create([param], (err) => {
+            if (!err) {
+                res.send({ code: 200, msg: '添加成功' })
+            } else {
+                res.send({ code: 301, msg: err });
+            }
+        })
     })
-  })
 
 })
 
-// 编辑数据
+// 修改数据
 router.post('/editInsideData', (req, res, next) => {
-  // table_id //总表的id    _id 该条数据的id
-  let query = req.body;
+    // table_id //总表的id    _id 该条数据的id
+    let query = req.body;
+    table.findOne({ _id: query.table_id }, {}, (err, docs) => {
+        let insideTableInfo = getInsideTableSchema(docs.tagSchema)
+        let nowModel = insideTable[('lineTableData' + docs._id)];
+        // 不存在这个 model 的时候 添加 model
+        if (!nowModel) {
+            insideTable[('lineTableData' + docs._id)] = mongoose.model('lineTableData' + docs._id, insideTableInfo);
+            nowModel = insideTable[('lineTableData' + docs._id)];
+        };
+        let param = {...query };
+        nowModel.find({ _id: param._id }, {}, (err, docs) => { //先查找是否有该条数据 再进行操作
+            if (docs.length !== 0) {
+                nowModel.updateOne({ _id: param._id }, {...param }, (err, docs) => {
+                    if (!err) {
+                        console.log('修改成功');
+                        res.send({ code: 200, msg: '修改成功' })
+                    } else {
+                        res.send({ code: 301, msg: '修改失败' });
+                        throw err;
+                    }
+                })
+            }
+        })
+    })
 
+})
+
+//  删除数据 
+router.post('/deleteInsideData', (req, res, next) => {
+    // table_id //总表的id    _id 该条数据的id
+    let query = req.body;
+    table.findOne({ _id: query.table_id }, {}, (err, docs) => {
+        let insideTableInfo = getInsideTableSchema(docs.tagSchema)
+        let nowModel = insideTable[('lineTableData' + docs._id)];
+        // 不存在这个 model 的时候 添加 model
+        if (!nowModel) {
+            insideTable[('lineTableData' + docs._id)] = mongoose.model('lineTableData' + docs._id, insideTableInfo);
+            nowModel = insideTable[('lineTableData' + docs._id)];
+        };
+        let param = {...query };
+        nowModel.find({ _id: param._id }, {}, (err, docs) => { //先查找是否有该条数据 再进行操作
+            if (docs.length !== 0) {
+                nowModel.deleteOne({ _id: param._id }, (err) => {
+                    if (!err) {
+                        res.send({ code: 200, msg: '删除' })
+                    } else {
+                        res.send({ code: 301, msg: '删除失败' });
+                        throw err;
+                    }
+                })
+            }
+        })
+    })
 })
 
 
