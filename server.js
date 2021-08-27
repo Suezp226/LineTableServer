cb4c6db0aefd70151abd7e77a416ee5dbbedea43
Suezp <769198688@qq.com>
2021-08-27 17:26:55 +0800

    加了一个 wx 获取 accesstoken 的接口

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+const request  =  require("request");
+
+
 // 各级路由
 const table = require("./src/tableList");
 const insideTable = require("./src/insideTable");
@@ -24,6 +27,14 @@ app.use(express.static(path.join(__dirname,'pages')));
 app.get('/api',(req,res)=>{
   res.send('数据库连接成功')
 })
+
+app.get('/wxJsAccesToken',(req,res)=>{
+  request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx908e307ce8863893&secret=f7127c00bee2837aa0a4ee1a9e0c348f',(err,resp,body)=>{
+    res.send(resp);
+  })
+
+})
+
 // 首页表
 app.use('/table',table.router)
 // 内部表数据
